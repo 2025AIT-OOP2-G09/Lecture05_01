b923c146bea263b5bca16b4d5b7b1fc072dfb36c
Haruimi <109784288+Haru-Sunflower@users.noreply.github.com>
2025-10-30 15:31:14 +0900

    😢

--- src/k24149.py
+++ src/k24149.py
@@ -7,11 +7,12 @@ def lecture05_01_k24149():
     # カメラキャプチャ実行
     app = MyVideoCapture()
     app.run()
+    
 
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
-    # capture_img : cv2.Mat = "implement me"
+    # capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
+    capture_img : cv2.Mat = cv2.imwrite('output_images/lecture05_01_k24149.png', capture_img)
 
     g_hight, g_width, g_channel = google_img.shape
     c_hight, c_width, c_channel = capture_img.shape
@@ -28,4 +29,6 @@ def lecture05_01_k24149():
 
     # 書き込み処理
     # implement me
+    cv2.imwrite('output_images/lecture05_01_k24149.png', capture_img)
+    print("キャプチャ画像を 'output_images/lecture05_01_k24149.png' に保存しました。")
 
